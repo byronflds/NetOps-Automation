5c258a7e664f2436ba9114bcdb3e3419e1a99bce
Byron Fields <byronflds@gmail.com>
2026-01-04 09:28:29 -0600

    update
    added field to port database model; vlan_id

--- app/models/port.py
+++ app/models/port.py
@@ -8,6 +8,7 @@ class Port(db.Model):
     port_name = db.Column(db.String(50), nullable=False)
     status = db.Column(db.String(20), nullable=False)
     is_uplink = db.Column(db.Boolean, default=False)
+    vlan_id = db.Column(db.Integer, nullable=True)
 
     def __repr__(self):
         return f"<Port {self.switch_name}:{self.port_name} ({self.status})>"
