74ade505b613fb2bb5404202e3a1dd90d33a9d4f
Byron Fields <byronflds@gmail.com>
2026-01-09 15:30:26 -0600

    Update audit_log.py

---
+++ app/models/audit_log.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from app.extensions import db
+
+class AuditLog(db.Model):
+    __tablename__ = "audit_logs"
+
+    id = db.Column(db.Integer, primary_key=True)
+    action = db.Column(db.String(100), nullable=False)
+    details = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    s
