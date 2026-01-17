50e54fcce9d019e20c6a86aee12fae6851d5d59f
Byron Fields <byronflds@gmail.com>
2026-01-17 17:20:29 -0600

    Update main.py

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template, current_app
+from flask_login import logout_user, login_required
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
+@login_required
 def index():
     current_app.logger.info("Index page accessed")
     return render_template("index.html")
